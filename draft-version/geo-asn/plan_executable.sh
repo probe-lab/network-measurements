b339e38f9040c4fe804f821732215e77d3c437eb
Dennis Trautwein <git@dtrautwein.eu>
2024-01-08 15:56:17 +0100

    repository cleanup '24

--- draft-version/geo-asn/plan_executable.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-./start_crawl
-aws s3 sync output_data_crawls/ s3://$bucketname
--- draft-version/observatory/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "servers" {
-  value = [aws_instance.server.*.public_ip]
-}
-
-output "public_ips_v6" {
-  value = [aws_instance.server.*.ipv6_addresses]
-}
